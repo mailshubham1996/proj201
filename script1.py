d1b00b4fa11ef02c6a376698cd87d23e8a94ce14
Shubham <mailshubham1996@gmail.com>
2018-06-24 17:44:56 +0530

    added basic codes to segment image/pca reduction

--- script1.py
+++ script1.py
@@ -1,4 +1,5 @@
-import numpy as np
+#PCA ASSESSMENT
+'''import numpy as np
 import cv2
 from matplotlib import pyplot as plt
 import os
@@ -8,10 +9,9 @@ from sklearn.decomposition import PCA
 os.getcwd()
 path="C:/proj/images/train/"
 desc=[]
-f = open('C:/proj/dataset.txt', 'wb')
-cen=np.zeros([1,3],dtype=int)
+
 for i,filename in enumerate(os.listdir(path)):
-e
+
     sift = cv2.xfeatures2d.SIFT_create()
     img1=cv2.imread(path+filename,0)
   #  img1=(img1).astype(np.uint8)
@@ -22,21 +22,85 @@ e
     #now cluseter the set of descriptors using k means
     #to have a constant number of vectors
     x=des1
-    pca=PCA(n_components=4)
+    pca=PCA(n_components=128)
     pca.fit(x)
     print(pca.singular_values_)
-     
+    #print(np.shape(des1))
     #centroids=centroids.transpose()
         
         #np.concatenate((cen,centroids),axis=0)
-    print(centroids)
-print(centroids.shape)#it should be (1,128)
-print('*'*40)
-print(cen)    
-
-    
 
 
-##write desc to txt file and add label to each of the desc rows 0 for diseased and 1 fornot
+#WATERSHED ALGORITHM
+import numpy as np
+import cv2
+from matplotlib import pyplot as plt
+path="C:/proj/images/test/64.png"
+img = cv2.imread(path)
+gray = cv2.cvtColor(img,cv2.COLOR_BGR2GRAY)
+ret, thresh = cv2.threshold(gray,0,255,cv2.THRESH_BINARY_INV+cv2.THRESH_OTSU)
+
+#oise removal
+kernel = np.ones((3,3),np.uint8)
+opening = cv2.morphologyEx(thresh,cv2.MORPH_OPEN,kernel, iterations = 2)
+# sure background area
+sure_bg = cv2.dilate(opening,kernel,iterations=3) 
+# Finding sure foreground area
+dist_transform = opening
+ret, sure_fg = cv2.threshold(dist_transform,0.7*dist_transform.max(),255,0) 
+# Finding unknown region
+sure_fg = np.uint8(sure_fg)
+unknown = cv2.subtract(sure_bg,sure_fg)
+
+
+
+ret, markers = cv2.connectedComponents(sure_fg) 
+# Add one to all labels so that sure background is not 0, but 1
+markers = markers
+# Now, mark the region of unknown with zero
+markers[unknown==255] = 0
+
+
+
+
+markers = cv2.watershed(img,markers)
+img[markers == -1] = [255,0,0]
+
+
+cv2.imshow(' ',img)
+cv2.waitKey(0)
+cv2.destroyAllWindows()
+
+'''
+
+
+#grab cut
+
+import numpy as np
+import cv2 as cv
+from matplotlib import pyplot as plt
+
+path="C:/proj/images/test/65.png"
+img = cv.imread(path)
+kret=img
+
+
+mask = np.zeros(img.shape[:2],np.uint8)
+bgdModel = np.zeros((1,65),np.float64)
+fgdModel = np.zeros((1,65),np.float64)
+
+
+rect = (10,10,600,400)
+cv.grabCut(img,mask,rect,bgdModel,fgdModel,5,cv.GC_INIT_WITH_RECT)
+mask2 = np.where((mask==2)|(mask==0),0,1).astype('uint8')
+img = img*mask2[:,:,np.newaxis]
+
+
+plt.imshow(img)
+
+plt.colorbar()
+plt.show()
+
+
 
 
