6fc431e22426e3e6d2b4ed728ac3eca13c52b6dc
Shubham <mailshubham1996@gmail.com>
2018-07-21 13:51:18 +0530

    added resized image part

--- file_renamer.py
+++ file_renamer.py
@@ -3,6 +3,15 @@ os.getcwd()
 collection = "C:/proj/images/train"
 for i, filename in enumerate(os.listdir(collection)):
     os.rename("C:/proj/images/train/" + filename, "C:/proj/images/train/" + str(i) + ".png")
+    image=cv2.imread(path+filename,0)
+    #r = 100.0 / image.shape[1]
+    #dim = (100, int(image.shape[0] * r))
+ 
+    # perform the actual resizing of the image and show it
+    resized = cv2.resize(image, (255,255))
+   #added the resized part.
+    cv2.waitKey(0)
+
               
               
               
