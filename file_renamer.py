1047e8ca461ce8f473c5d95b5786a79ecc532581
Shubham <mailshubham1996@gmail.com>
2018-07-24 12:05:47 +0530

    fixed errors

--- file_renamer.py
+++ file_renamer.py
@@ -7,8 +7,13 @@ for i, filename in enumerate(os.listdir(path)):
     image=cv2.imread(path+filename,0)
     #r = 100.0 / image.shape[1]
     #dim = (100, int(image.shape[0] * r))
- 
+    index=0
 # perform the actual resizing of the image and show it
+    s1=" "
+    s1=filename
+    index=s1.find(".")
+    s1=s1[0:index]
+   
     resized = cv2.resize(image, (255,255))
     cv2.imshow("resized", resized)
     s=" "
@@ -18,4 +23,4 @@ for i, filename in enumerate(os.listdir(path)):
     cv2.waitKey(0)
 
 
-    os.rename("C:/proj/images/train/" + filename, "C:/proj/images/train/" + str(i) + ".png")
+    os.rename("C:/proj/images/train/" + filename, "C:/proj/images/train/" + s1 + ".png")
